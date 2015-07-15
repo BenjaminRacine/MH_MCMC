bc390a99bad70b595018d69a740b173e7164c6a9
Benjamin Racine <benjar@reinette.local>
2015-07-15 20:38:46 +0200

    updated the log version of the MH

--- MH_module.py
+++ MH_module.py
@@ -86,23 +86,70 @@ def MCMC(guess,functional_form,proposal,proposal_fun,niter,*arg):
     niter -- number of iterations
     *arg -- arguments that could be used by the functional form, and the proposal: *arg[0] for the functional, *arg[1] for the proposal. It can be for example *arg = [A,x],[]
     """
+    acceptance = 0
     tests = []
     i=0
     pi_out = functional_form(guess,*arg[0])
     tests.append((guess))
     while i<niter:
         guess_new = guess + proposal(*arg[1])
-        A = min(1,functional_form(guess_new,*arg[0])/functional_form(guess,*arg[0])*proposal_fun(guess-guess_new,*arg[1])/proposal_fun(guess_new-guess,*arg[1]))
+        A = min(1,functional_form(guess_new,*arg[0])/functional_form(guess,*arg[0])*proposal_fun(guess,guess_new,*arg[1])/proposal_fun(guess_new,guess,*arg[1]))
         #### will have to implement a log 
         if A==1:
             guess=guess_new
             tests.append((guess))
+            acceptance+=1
         elif A<1:
             u = np.random.rand(1)
             if u <= A:
                 guess=guess_new
                 tests.append((guess))
+                acceptance+=1
             else:
                 pass
         i+=1
+    print "acceptance rate = ",float(acceptance)/niter
+    return tests
+
+
+def MCMC_log(guess,functional_form,proposal,proposal_fun,niter,*arg):
+    """
+    Same as previous, but for log likelihood
+
+    Keyword Arguments:
+    guess -- initial guess vector (np.array)
+    functional_form -- form you are trying to sample
+    proposal -- proposal random generator for next step
+    proposal_fun -- proposal function to calculate ratios (next version should have both proposal and proposal_fun in one go)
+    niter -- number of iterations
+    *arg -- arguments that could be used by the functional form, and the proposal: *arg[0] for the functional, *arg[1] for the proposal. It can be for example *arg = [A,x],[]
+    """
+    acceptance = 0
+    tests = []
+    i=0
+    f_old = functional_form(guess,*arg[0])
+    tests.append((guess))
+    while i<niter:
+        print i
+        guess_new = guess + proposal(*arg[1])
+        f_new = functional_form(guess_new,*arg[0])
+        A = min(0,f_new-f_old+proposal_fun(guess,guess_new,*arg[1])-proposal_fun(guess_new,guess,*arg[1]))
+        print A,f_new
+        #### will have to implement a log 
+        if A==0:
+            guess=guess_new
+            tests.append((guess))
+            acceptance+=1
+            f_old = f_new
+        elif A<0:
+            u = np.log(np.random.rand(1))
+            if u <= A:
+                guess=guess_new
+                tests.append((guess))
+                acceptance+=1
+                f_old = f_new
+            else:
+                pass
+        i+=1
+    print "acceptance rate = ",float(acceptance)/niter
     return tests

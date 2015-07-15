ef1f9dc49dbdbacbb8b70cb0336e30ce60b6091e
Benjamin Racine <benjar@reinette.local>
2015-07-15 20:44:11 +0200

    first upload

---
+++ ploter_parameters.py
@@ -0,0 +1,76 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from matplotlib.ticker import NullFormatter
+from matplotlib.patches import Ellipse  
+
+
+
+def Triangle_plot_Cov(Cov,x_mean,**kwargs):
+    """
+    """
+    
+    nullfmt   = NullFormatter()
+    nb_param = Cov.shape[0]
+    # definitions for the axes (left width and left_h is also bottom height and bottom_h)
+    left = 0.1/(nb_param)
+    width = 0.9/(nb_param) 
+    axHistx=[]
+    axScatter=[]
+    for i in range(nb_param):
+        rect_histx = [left+i*width, left+(nb_param-1-i)*width, width, width]
+        ax_temp = plt.axes(rect_histx)
+        axHistx.append(ax_temp)
+        ax_temp.xaxis.tick_top()
+        ax_temp.yaxis.set_visible(False)#tick_right()
+        ax_temp.locator_params(nbins=4)
+        x1 = np.linspace(x_mean[i]- 3*np.sqrt(Cov[i,i]),x_mean[i] + 3*np.sqrt(Cov[i,i]),200)
+        #sUtahtop
+        ax_temp.plot(x1,np.exp(-0.5*(x1-x_mean[i])**2/Cov[i,i])/np.sqrt(2*np.pi*Cov[i,i]),**kwargs)
+        for j in range(i+1,nb_param):
+            rect_scatter = [left+(i)*width, left+(nb_param-j-1)*width, width, width]
+            #print 1
+            ax_temp=plt.axes(rect_scatter)
+            ell = plot_ellipse(Cov[[i,j],:][:,[i,j]],x_mean[[i,j]],1,plot=1,axe=ax_temp,fill=False)
+#            ax_temp.add_artist(ell)
+            #ax_temp.plot(np.arange(12))
+            ax_temp.xaxis.set_major_formatter(nullfmt)
+            ax_temp.xaxis.set_visible(False)
+            ax_temp.yaxis.set_visible(False)
+            axScatter.append(ax_temp)
+            pass
+    return axScatter, axHistx
+
+
+def plot_ellipse(cov,x_mean,nsig,axe,plot=0,**kwargs):
+    def eigsorted(cov):
+        vals, vecs = np.linalg.eigh(cov)
+        order = vals.argsort()[::-1]
+        return vals[order], vecs[:,order]
+    vals, vecs = eigsorted(cov)
+    theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+    width, height = 2 * nsig * np.sqrt(vals)
+    ell = Ellipse(xy=x_mean, width=width, height=height, angle=theta,**kwargs)
+    #plt.figure()
+    #ax = plt.gca()
+    if plot == 1:
+        axe.add_artist(ell)
+        print ell.get_extents()
+        plt.xlim(ell.center[0]-3*np.sqrt(cov[0,0]),ell.center[0]+3*np.sqrt(cov[0,0]))
+        plt.ylim(ell.center[1]-3*np.sqrt(cov[1,1]),ell.center[1]+3*np.sqrt(cov[1,1]))
+    #stop
+    return ell
+
+
+def cor2cov(cov_diag,Correlation_matrix):
+    """
+    Computes the covariance matrix, given the correlation matrix and the diagonal elements of the covariance
+    """
+    dim = cov_diag.shape[0]
+    cov_new = np.zeros((dim,dim))
+    for i in range(dim):
+        #print i
+        for j in range(i,dim):
+            #print j
+            cov_new[i,j] = Correlation_matrix[i,j]/100.*np.sqrt(cov_diag[i]*cov_diag[j])
+            cov_new[j,i] = cov_new[i,j]
+    return cov_new

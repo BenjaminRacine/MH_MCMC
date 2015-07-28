55c8d1fd94f5f2623d3477d0705302128d750d7e
Benjamin Racine <benjar@reinette.local>
2015-07-28 22:27:24 +0200

    Triangle scatter plot added

--- ploter_parameters.py
+++ ploter_parameters.py
@@ -54,7 +54,7 @@ def plot_ellipse(cov,x_mean,nsig,axe,plot=0,**kwargs):
     #ax = plt.gca()
     if plot == 1:
         axe.add_artist(ell)
-        print ell.get_extents()
+        #print ell.get_extents()
         plt.xlim(ell.center[0]-3*np.sqrt(cov[0,0]),ell.center[0]+3*np.sqrt(cov[0,0]))
         plt.ylim(ell.center[1]-3*np.sqrt(cov[1,1]),ell.center[1]+3*np.sqrt(cov[1,1]))
     #stop
@@ -74,3 +74,45 @@ def cor2cov(cov_diag,Correlation_matrix):
             cov_new[i,j] = Correlation_matrix[i,j]/100.*np.sqrt(cov_diag[i]*cov_diag[j])
             cov_new[j,i] = cov_new[i,j]
     return cov_new
+
+
+
+
+def Triangle_plot_Cov_dat(guesses,flag,x_mean,Cov,**kwargs):
+    """
+    """
+    
+    nullfmt   = NullFormatter()
+    nb_param = guesses.shape[1]
+    # definitions for the axes (left width and left_h is also bottom height and bottom_h)
+    left = 0.1/(nb_param)
+    width = 0.9/(nb_param) 
+    axHistx=[]
+    axScatter=[]
+    for i in range(nb_param):
+        rect_histx = [left+i*width, left+(nb_param-1-i)*width, width, width]
+        ax_temp = plt.axes(rect_histx)
+        axHistx.append(ax_temp)
+        ax_temp.xaxis.tick_top()
+        ax_temp.yaxis.set_visible(False)#tick_right()
+        ax_temp.locator_params(nbins=4)
+        x1 = np.linspace(x_mean[i]- 3*np.sqrt(Cov[i,i]),x_mean[i] + 3*np.sqrt(Cov[i,i]),200)
+        #sUtahtop
+        ax_temp.plot(x1,np.exp(-0.5*(x1-x_mean[i])**2/Cov[i,i])/np.sqrt(2*np.pi*Cov[i,i]),**kwargs)
+        ax_temp.hist(guesses[:,i][flag>0],np.sqrt(sum(flag>0)),histtype="step",normed=True)
+        for j in range(i+1,nb_param):
+            rect_scatter = [left+(i)*width, left+(nb_param-j-1)*width, width, width]
+            print j
+            ax_temp=plt.axes(rect_scatter)
+            ell = plot_ellipse(Cov[[i,j],:][:,[i,j]],x_mean[[i,j]],1,plot=1,axe=ax_temp,fill=False)
+            ax_temp.scatter(guesses[:,i][flag==0],guesses[:,j][flag==0],color="k",alpha=0.05)
+            ax_temp.scatter(guesses[:,i][flag==1],guesses[:,j][flag==1],color="g",alpha=0.3)
+            ax_temp.scatter(guesses[:,i][flag==2],guesses[:,j][flag==2],color="r",alpha=0.3)
+#            ax_temp.add_artist(ell)
+            #ax_temp.plot(np.arange(12))
+            ax_temp.xaxis.set_major_formatter(nullfmt)
+            ax_temp.xaxis.set_visible(False)
+            ax_temp.yaxis.set_visible(False)
+            axScatter.append(ax_temp)
+            pass
+    return axScatter, axHistx

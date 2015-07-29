9fb7bde76a36f9ae388568ea23d094367766191e
Benjamin Racine <benjar@reinette.local>
2015-07-29 02:46:51 +0200

    save chains (2)

--- parameter_sampling_planck2015_H0.py
+++ parameter_sampling_planck2015_H0.py
@@ -87,7 +87,7 @@ def plot_input(dlm):
 
 
 
-def run_MCMC(which_par,niter):
+def run_MCMC(which_par,niter,save_title):
     cov_new_temp = cov_new[which_par,:][:,which_par]
     string_temp = strings[which_par]
     titles_temp = titles[which_par]
@@ -97,7 +97,7 @@ def run_MCMC(which_par,niter):
     guess_param = PS2P.prop_dist_form_params(x_mean_temp,cov_new_temp)
     testss = np.array(MH.MCMC_log_new_priors(guess_param, PS2P.functional_form_params_n,PS2P.prop_dist_form_params, PS2P.prop_func_form_params,niter,forced_priors_temp,[dlm,string_temp,dd,nl,bl],[x_mean_temp*0,np.matrix(cov_new_temp)]))
     #print "%.2f rejected; %.2f accepted; %.2f Lucky accepted"%((flag==0).mean(),(flag==1).mean(),(flag==2).mean())
-    np.save("chain_%s_%d_%d.npy"%(str(which_par).replace(',','').replace('[','').replace(']','').replace(' ',''),np.random.randint(0,100000),niter),testss)
+    np.save("chain_%s_%s_%d_%d.npy"%(save_title,str(which_par).replace(',','').replace('[','').replace(']','').replace(' ',''),np.random.randint(0,100000),niter),testss)
     return testss
 
 def plot_chains(guesses,flag,titles,which_par):
